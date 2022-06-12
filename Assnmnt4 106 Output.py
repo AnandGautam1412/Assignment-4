483d4ba95a7aa29c1742d2ca771f02794bd0f68d
AnandGautam1412 <104383379+AnandGautam1412@users.noreply.github.com>
2022-06-12 23:32:45 +0530

    Add files via upload

---
+++ Assnmnt4 106 Output.py
@@ -0,0 +1,33 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:/Users/asus/OneDrive/Desktop/Assnmnt4 106.py ===========
+Q1
+Enter mark:81
+Grade: A
+Q2
+Enter year:2654
+Not a leap year
+Q3
+Question 1:5x3=15
+Right!
+Question 2:7x9=56
+Wrong. The correct answer is 63
+Question 3:4x2=8
+Right!
+Question 4:6x2=12
+Right!
+Question 5:7x7=66
+Wrong. The correct answer is 49
+Question 6:8x5=65
+Wrong. The correct answer is 40
+Question 7:5x7=52
+Wrong. The correct answer is 35
+Question 8:8x6=48
+Right!
+Question 9:4x8=32
+Right!
+Question 10:6x7=55
+Wrong. The correct answer is 42
+Q4
+Answer: 177
